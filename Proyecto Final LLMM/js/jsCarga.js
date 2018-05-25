0fb765079d273f11fb4b52c2a7ccaf029d4b115e
Unknown <andrivgar@gmail.com>
2018-05-25 17:57:56 +0200

    Proyecto final 3

---
+++ Proyecto Final LLMM/js/jsCarga.js
@@ -0,0 +1,15 @@
+
+$.getJSON('../data/1.json', function(datos) {
+    var totalNoticias = Object.keys(datos.data).length;
+});
+
+function cargarNoticias(){
+    var numeroNoticia = 0;
+
+    // $.getJSON("../data/1.json", function(data){
+        
+    // })
+
+
+
+}
